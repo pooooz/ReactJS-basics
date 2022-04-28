31c695e2d31c5ef3d6d1f500ffb9b20983a35d19
POZ <antoivans@gmail.com>
2022-04-28 03:41:47 +0300

    Solution of the fifth task

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore, compose } from 'redux';
+import { profileReducer } from 'src/store/profile/reducer';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(profileReducer, composeEnhancers());
